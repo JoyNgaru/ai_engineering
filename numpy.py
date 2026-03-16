3cb8c29b6e1f080cf831c6fb4dbb7909e48e2d54
Joy  Ngaru <joywanjikum@gmail.com>
2026-03-16 11:07:51 +0300

    Add basic numpy operations in numpy.py

---
+++ numpy.py
@@ -0,0 +1,7 @@
+import numpy as np
+v = np.array([1, 2, 3])
+print("Double :", v * 2)  # Output: [2 4 6]
+print("Square :", v ** 2)  # Output: [1 4 9
+print("Sum :", np.sum(v))  # Output: 6
+print("Mean :", np.mean(v))  # Output: 2.0
+print("Dot Product :", np.dot(v, v))  # Output: 14
